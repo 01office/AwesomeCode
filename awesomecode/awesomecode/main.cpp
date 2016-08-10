67ee54c16714e3931d1c0c9b9371b94c2366336e
Neo He <neohe2007@gmail.com>
2016-08-11 00:17:40 +0800

    bubble sort 1

--- awesomecode/awesomecode/main.cpp
+++ awesomecode/awesomecode/main.cpp
@@ -7,9 +7,41 @@
 //
 
 #include <iostream>
+using namespace std;
+
+void myswap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void myprint(int arr[], int n)
+{
+    for (int index = 0; index < n; ++index) {
+        cout << arr[index] << " ";
+    }
+    cout << endl;
+}
+
+// 冒泡排序
+void bubble_sort_1(int arrry[], int n)
+{
+    for (int i = 0; i < n; ++i) {
+        for (int j = 1; j < n - i; ++j) {
+            if (arrry[j - 1] > arrry[j]) {
+                myswap(arrry[j - 1], arrry[j]);
+            }
+        }
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[] = {5, 7, 2, 4, 10, 88, 22, 13, 66, 11};
+    
+    bubble_sort_1(a, 10);
+    
+    myprint(a, 10);
+    
     return 0;
 }
