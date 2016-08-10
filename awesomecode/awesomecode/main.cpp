e6dd132301d03d3ef636a93971c6b7efdee2ed21
Neo He <neohe2007@gmail.com>
2016-08-11 00:03:49 +0800

    init project

---
+++ awesomecode/awesomecode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  awesomecode
+//
+//  Created by heyong on 16/8/11.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
