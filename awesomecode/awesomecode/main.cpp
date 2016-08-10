bc8deb6cdb9d2e2b3fb13c81a3a64b234d05731b
Neo He <neohe2007@gmail.com>
2016-08-11 00:37:21 +0800

    improved bubble sort

--- awesomecode/awesomecode/main.cpp
+++ awesomecode/awesomecode/main.cpp
@@ -24,7 +24,12 @@ void myprint(int arr[], int n)
     cout << endl;
 }
 
-// 冒泡排序
+/**
+ * 冒泡排序
+ * 1. 比较两个相邻的数据，如果前面的大于后面的，则交换
+ * 2. 对数组第 0 到 n-1 遍历一遍后，最大的数据将会到达数组第 n-1 的位置
+ * 3. n=n-1，如果 n 不为 0，则重复第二步
+ */
 void bubble_sort_1(int arrry[], int n)
 {
     for (int i = 0; i < n; ++i) {
@@ -36,10 +41,32 @@ void bubble_sort_1(int arrry[], int n)
     }
 }
 
+/**
+ * 改进的冒泡排序
+ * 设置标志，如果一趟发生了交换，则置标志为 true，否则为 false。
+ * 如果有一趟没有发生交换，则排序已经完成
+ */
+void bubble_sort_2(int array[], int n)
+{
+    int k = n, j;
+    bool flag = true;
+    
+    while (flag) {
+        flag = false;
+        for (j = 1; j < k; ++j) {
+            if (array[j - 1] > array[j]) {
+                myswap(array[j - 1], array[j]);
+                flag = true;
+            }
+        }
+        k--;
+    }
+}
+
 int main(int argc, const char * argv[]) {
     int a[] = {5, 7, 2, 4, 10, 88, 22, 13, 66, 11};
     
-    bubble_sort_1(a, 10);
+    bubble_sort_2(a, 10);
     
     myprint(a, 10);
     
