e9a5bcd47afae838edb6e2877c6a4239d21f12c3
Neo He <neohe2007@gmail.com>
2016-08-30 22:24:56 +0800

    add files

---
+++ awesomecode/awesomecode/my_algor.cpp
@@ -0,0 +1,9 @@
+//
+//  my_algor.cpp
+//  awesomecode
+//
+//  Created by heyong on 16/8/30.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#include "my_algor.hpp"
---
+++ awesomecode/awesomecode/my_algor.hpp
@@ -0,0 +1,14 @@
+//
+//  my_algor.hpp
+//  awesomecode
+//
+//  Created by heyong on 16/8/30.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#ifndef my_algor_hpp
+#define my_algor_hpp
+
+#include <stdio.h>
+
+#endif /* my_algor_hpp */
