08b4626a1afd56076608c12bcf437a651e83ceb3
Neo He <neohe2007@gmail.com>
2016-12-10 21:29:21 +0800

    begin my singleton

---
+++ awesomecode/awesomecode/my_singleton.cpp
@@ -0,0 +1,9 @@
+//
+//  my_singleton.cpp
+//  awesomecode
+//
+//  Created by heyong on 2016/12/10.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#include "my_singleton.hpp"
---
+++ awesomecode/awesomecode/my_singleton.hpp
@@ -0,0 +1,14 @@
+//
+//  my_singleton.hpp
+//  awesomecode
+//
+//  Created by heyong on 2016/12/10.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#ifndef my_singleton_hpp
+#define my_singleton_hpp
+
+#include <stdio.h>
+
+#endif /* my_singleton_hpp */
