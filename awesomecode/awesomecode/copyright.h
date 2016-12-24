7a7dae9f3a66bb82c42234b8497207868a7d337a
Neo He <neohe2007@gmail.com>
2016-12-24 20:43:40 +0800

    copyright

---
+++ awesomecode/awesomecode/copyright.h
@@ -0,0 +1,16 @@
+//
+//  copyright.h
+//  awesomecode
+//
+//  Created by heyong on 2016/12/24.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#ifndef copyright_h
+#define copyright_h
+
+/*
+ * Copyright (c) 2016, Neo He <neohe2007 at gmail dot com>
+ */
+
+#endif /* copyright_h */
